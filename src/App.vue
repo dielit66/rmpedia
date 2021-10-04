c642e92f22ab39850a190661fe91c887cc73a304
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2021-10-04 21:29:49 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: { Header },
+};
+</script>
+
+<style>
+#app {
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <img
+      class="logo_name"
+      src="../assets/logoName.png"
+      alt="Rick and Morty Logo"
+    />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+}
+.logo_name {
+  width: 350px;
+  margin: 20px auto 0;
+}
+</style>
