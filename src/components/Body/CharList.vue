697df670471498633eb93856853164b4fa114274
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2021-10-05 22:19:01 +0300

    +characters list
    Добавлен список персонажей на главную страницу

--- src/components/Body/CharList.vue
+++ src/components/Body/CharList.vue
@@ -1,7 +1,21 @@
 <template>
   <main>
-    <div class="app-wrapper">
-      <img :src="info" alt="Rick" />
+    <div class="all-characters">
+      <div
+        class="character_card"
+        v-for="character in characters"
+        :key="character.id"
+      >
+        <div class="character_info">
+          <img class="character_image" :src="character.image" alt="" />
+          <div class="character_name">{{ character.name }}</div>
+          <div class="character_add-info">
+            <span>{{ character.gender }}</span>
+            <span>{{ character.status }}</span>
+            <span>{{ character.species }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -13,15 +27,47 @@ export default {
   name: "CharList",
   data() {
     return {
-      info: null,
+      characters: [],
     };
   },
   mounted() {
     axios
-      .get("https://rickandmortyapi.com/api/character/1")
-      .then((response) => (this.info = response.data.image));
+      .get("https://rickandmortyapi.com/api/character/")
+      .then((response) => (this.characters = response.data.results));
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.all-characters {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 5%;
+}
+.character_image {
+  margin: 0 auto;
+}
+
+.character_card {
+  background-color: #00e3ff;
+  width: 30%;
+  margin-top: 35px;
+}
+.character_info {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+.character_name {
+  text-align: center;
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+.character_add-info {
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 16px;
+  padding-bottom: 16px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import axios from "axios";
+import VueRouter from "vue-router";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store, axios, VueRouter).mount("#app");
